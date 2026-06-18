053ef7d7815b46defeafbc327e9d8714c0113a4f
Mohamed Benjbara <mbenjbar@c3r9p2.1337.ma>
2026-06-18 19:49:15 +0100

    started USER

--- Commands/user.cpp
+++ Commands/user.cpp
@@ -1,3 +1,29 @@
 #include "../Server/Server.hpp"
 
-void    Server::User(Client &, std::string receive) {std::cout << "merci hhh\n"; (void)receive;}
+std::string &get_realname(std::stringstream &ss)
+{
+
+}
+
+void    Server::User(Client &current, std::string receive)
+{
+    std::stringstream ss(receive);
+    std::string command;
+    std::string username;
+    std::string realname;
+
+    ss >> command;
+    ss >> username;
+
+
+    if (!current.is_passed())
+        return (current.send_msg(":localhost " + ERR_NOTREGISTERED + current.get_Nickname() + " USER :You have not registered\r\n"));
+    
+    if (current.is_Registered() || current.hasUser())
+        return (current.send_msg(":localhost " + ERR_ALREADYREGISTRED + current.get_Nickname() + " USER :Unauthorized command, already registered\r\n"));
+
+    if (username.empty())
+        return (current.send_msg(":localhost " + ERR_NEEDMOREPARAMS + current.get_Nickname() + " USER :Not enough parameters\r\n"));
+
+    realname = get_realname(ss);
+}
